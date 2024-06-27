688766affb31d190b78d6aa8cfd63ee8b1129fb6
fedka1979 <fedka-3@yandex.ru>
2024-06-27 18:06:46 +0300

    Add files via upload

---
+++ Homework3.py
@@ -0,0 +1,9 @@
+name = 'Андрей'
+print(name, type(name))
+age = 44
+print(age, type(age))
+new_age = 45
+print(new_age, type(new_age))
+print(new_age - age)
+is_student = 'true'
+print(is_student, type(is_student))
