92b48fc3c99a54361435ffd974e221266be0226e
fedka1979 <fedka-3@yandex.ru>
2024-07-01 20:34:56 +0300

    Update Homework3.py

--- Homework3.py
+++ Homework3.py
@@ -2,8 +2,8 @@ name = 'Андрей'
 print(name, type(name))
 age = 44
 print(age, type(age))
-new_age = 45
+new_age = 1
 print(new_age, type(new_age))
-print(new_age - age)
-is_student = 'true'
+print(new_age + age)
+is_student = True
 print(is_student, type(is_student))
